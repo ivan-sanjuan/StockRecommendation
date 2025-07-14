565306d7ea829007882893b8e62dbf12b52d59ec
vashmeow <isanjuanivan@gmail.com>
2025-07-15 06:21:08 +0800

    added a class for Stock

--- StockRecommendation.py
+++ StockRecommendation.py
@@ -7,12 +7,14 @@ from ph_scraper import scrape_and_parse
 
 stock_info = scrape_and_parse()
 
-if stock_info and len(stock_info) > 0:
+if  stock_info  and len(stock_info) > 0:
     cmpy_id = stock_info[0]['cmpy_id']
     security_id = stock_info[0]['security_id']
+    company_name = stock_info[0]['company_name']
     url = f'https://edge.pse.com.ph/companyPage/stockData.do?cmpy_id={cmpy_id}&security_id={security_id}'
     response = requests.get(url)
     html_content = response.text
+
     soup = BeautifulSoup(html_content, 'html.parser')
     tables = soup.find_all('table', class_='view')
     stock_table = tables[1]
@@ -25,7 +27,26 @@ if stock_info and len(stock_info) > 0:
             key = label.get_text(strip=True)
             value = cell.get_text(strip=True)
             stock_data[key] = value
-    pprint.pprint(stock_data)
+    #pprint.pprint(stock_data)
 
 else:
     print('No matching stock found.')
+    
+
+class Stock():
+    def __init__(self,stock_info,stock_data):
+        self.name = stock_info[0]['company_name']
+        self.open = stock_data.get('Open', 'N/A')
+        self.close = stock_data.get('Last Traded Price', 'N/A')
+
+    def __str__(self):
+        return f'''
+        Company: {self.name}
+        Open: {self.open}
+        Close: {self.close}
+        '''
+
+company = Stock(stock_info, stock_data)
+
+print(company)
+
