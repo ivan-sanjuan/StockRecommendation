07e3ee7644e0c439c7139dc1619b6a02de07111d
vashmeow <isanjuanivan@gmail.com>
2025-07-17 07:21:47 +0800

    minor update on the code

--- ph_stock_scraper.py
+++ ph_stock_scraper.py
@@ -1,20 +1,23 @@
 import json
-from stock_utils import input_stock
 
 with open(r'stock_directory.json', 'r') as file:
     stock_directory = json.load(file)
     
     results = []
-    def scrape_and_parse(stock_code):
-        input_stock = stock_code
-        for stock_symbol in stock_directory:
-            if stock_symbol.get('stock_symbol') == input_stock:
-               cmpy_id = stock_symbol.get('cmpy_id')
-               security_id = stock_symbol.get('security_id')
-               company_name = stock_symbol.get('company_name')
-               results.append({
-                   'cmpy_id': cmpy_id,
-                   'security_id': security_id,
-                   'company_name': company_name
-               })
-        return results
+    def scrape_and_parse(raw_input_data):
+        input_stock = raw_input_data
+        for stock in stock_directory:
+            if stock.get('stock_symbol') == input_stock:
+                cmpy_id = stock.get('cmpy_id')
+                security_id = stock.get('security_id')
+                company_name = stock.get('company_name')
+                results.append({
+                    'cmpy_id': cmpy_id,
+                    'security_id': security_id,
+                    'company_name': company_name
+                })
+        return results        
+    
+        
+
+                
